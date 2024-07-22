007f53701b260c2706ab8798d330e540181017d3
Ryu <r.lippmann@aims.gov.au>
2024-07-22 16:26:04 +1000

    Cluster targets
    Cluster suitable target locations be centroid

--- src/MS-T_routing.jl
+++ src/MS-T_routing.jl
@@ -11,3 +11,23 @@ function extract_subset(spatial_dataset::Raster, subset)
     result_raster = Rasters.trim(mask(spatial_dataset; with=subset.geom))
     return result_raster
 end
+
+"""
+    cluster_targets(df::DataFrame, num_clust::Int64)
+
+Cluster the targets in a GeoDataFrame based on their geometry.
+"""
+function cluster_targets(df::DataFrame, num_clust::Int64)
+    # Calculate centroid of geometry for each row
+    centroid_shp = [AG.centroid(row.geom) for row in eachrow(df)]
+    centroid_coords = [(AG.getx(centroid,0), AG.gety(centroid,0)) for centroid in centroid_shp]
+
+    # Convert the coordinates to a format suitable for clustering (e.g., an array)
+    coordinates_array = hcat([collect(c) for c in centroid_coords]...)
+
+    # Cluster centroids using kmeans
+    clustering = kmeans(coordinates_array, num_clust)
+
+    df.cluster_id = clustering.assignments
+    return df
+end
--- src/dev.jl
+++ src/dev.jl
@@ -11,3 +11,16 @@ bathy_dataset = Raster("../data/bathy/Cairns-Cooktown/Cairns-Cooktown_bathy.tif"
 target_bathy = extract_subset(bathy_dataset, suitable_targets)
 plot(target_bathy)
 write("../outputs/bathy_subset.tif", target_bathy; force=true)
+
+# Cluster targets
+
+k = 3
+cluster_targets(suitable_targets, k)
+GDF.write(
+    "../outputs/clustered_targets.gpkg",
+    suitable_targets[:, [:geom, :site_id, :UNIQUE_ID, :Reef, :area, :habitat,:cluster_id]],
+    geom_columns = (:geom,),
+    crs = EPSG(7844)
+)
+
+depot = (-16.92, 145.78) # (lat, long) = (y, x)
