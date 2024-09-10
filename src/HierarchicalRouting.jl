01e4ab8bca2ad62f36b41eedfdc051ea7c126054
Ryu <r.lippmann@aims.gov.au>
2024-09-10 16:39:09 +1000

    Unionize_overlaps to reduce number of polygons
    Combine overlapping polygons with unions.
    Remove overlapping polygon to reduce number df rows

--- src/HierarchicalRouting.jl
+++ src/HierarchicalRouting.jl
@@ -56,6 +56,48 @@ function process_exclusions(exclusions::DataFrame; buffer_dist::Real=1.0, simpli
     return
 end
 
+function unionize_overlaps(exclusions::DataFrame)
+    geometries = exclusions.geometry
+    n = length(geometries)
+    processed = falses(n)
+
+    for i in 1:n
+        if processed[i]
+            continue
+        end
+
+        geom1 = geometries[i]
+        for j in i+1:n
+            if processed[j]
+                continue
+            end
+
+            geom2 = geometries[j]
+            if AG.overlaps(geom1, geom2)
+                println("Overlap between $i and $j")
+
+                union_geom = AG.union(geom1, geom2)
+
+                exclusions.geometry[i] = union_geom
+                exclusions.geometry[j] = union_geom
+
+                for k in 1:n
+                    if AG.overlaps(union_geom, geometries[k]) && !processed[k]
+                        exclusions.geometry[k] = union_geom
+                        processed[k] = true
+                    end
+                end
+
+            end
+        end
+    end
+
+    # Remove duplicate unionized geometries
+    unique_geometries = unique(exclusions.geometry[.!AG.isempty.(exclusions.geometry)])
+    exclusions = DataFrame(geometry = unique_geometries)
+
+    return exclusions
+end
 
 include("analysis/clustering.jl")
 include("analysis/routing_heuristics.jl")
@@ -72,7 +114,8 @@ export
 export
     to_multipolygon,
     to_dataframe,
-    process_exclusions
+    process_exclusions,
+    unionize_overlaps
 
 export
     create_exclusion_zones,
