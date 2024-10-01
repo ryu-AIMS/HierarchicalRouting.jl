1abb55a862521ffe3f27e1e953b885c47310d6bf
Ryu <r.lippmann@aims.gov.au>
2024-10-01 16:53:04 +1000

    Relocate extract_subset() and target_threshold()

--- src/analysis/clustering.jl
+++ src/analysis/clustering.jl
@@ -1,37 +1,5 @@
 
 """
-    extract_subset(spatial_dataset::Raster, subset)
-
-Extract a subset from the given `spatial_dataset` raster based on the specified `subset`.
-
-# Arguments
-- `spatial_dataset::Raster`: The spatial dataset from which to extract the subset.
-- `subset`: The subset criteria to apply.
-
-# Returns
-A new raster containing the subset of the original raster.
-"""
-function extract_subset(spatial_dataset::Raster, subset)
-    return Rasters.trim(mask(spatial_dataset; with=subset.geom))
-end
-
-"""
-    target_threshold(target_bathy::Raster, ms_depth)
-
-Mask the target environmental raster based on the minimum threshold value.
-
-# Arguments
-- `target_bathy::Raster`: The target bathymetry raster.
-- `ms_depth`: The minimum depth threshold.
-"""
-function target_threshold(targets::Raster, threshold::Float64)
-    # TODO: add min/max argument flag, or use a range
-    # TODO: include masks for other environmental constraints
-    masked_raster = targets .* (targets .>= threshold)
-    return masked_raster
-end
-
-"""
     cluster_targets(df::DataFrame, num_clust::Int64)
 
 Cluster the targets in a GeoDataFrame based on their geometry.
--- src/analysis/processing.jl
+++ src/analysis/processing.jl
@@ -98,3 +98,35 @@ function unionize_overlaps!(exclusions::DataFrame)
 
     return exclusions
 end
+
+"""
+    extract_subset(spatial_dataset::Raster, subset)
+
+Extract a subset from the given `spatial_dataset` raster based on the specified `subset`.
+
+# Arguments
+- `spatial_dataset::Raster`: The spatial dataset from which to extract the subset.
+- `subset`: The subset criteria to apply.
+
+# Returns
+A new raster containing the subset of the original raster.
+"""
+function extract_subset(spatial_dataset::Raster, subset)
+    return Rasters.trim(mask(spatial_dataset; with=subset.geom))
+end
+
+"""
+    target_threshold(target_bathy::Raster, ms_depth)
+
+Mask the target environmental raster based on the minimum threshold value.
+
+# Arguments
+- `target_bathy::Raster`: The target bathymetry raster.
+- `ms_depth`: The minimum depth threshold.
+"""
+function target_threshold(targets::Raster, threshold::Float64)
+    # TODO: add min/max argument flag, or use a range
+    # TODO: include masks for other environmental constraints
+    masked_raster = targets .* (targets .>= threshold)
+    return masked_raster
+end
