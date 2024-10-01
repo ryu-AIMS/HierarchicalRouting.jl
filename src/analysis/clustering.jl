d28cb97b8494af4ddbbff4a3266338397e5623a2
Ryu <r.lippmann@aims.gov.au>
2024-10-01 14:18:23 +1000

    multiple dispatch centroids() for cases without depot

--- src/analysis/clustering.jl
+++ src/analysis/clustering.jl
@@ -64,7 +64,7 @@ function cluster_targets(raster::Raster{Int16, 2}, num_clust::Int64)
 end
 
 """
-    centroids(cluster_raster::Raster{Int16, 2}, depot::Tuple{Float64, Float64})
+    centroids(clusters::Raster{Int16, 2}, depot::Tuple{Float64, Float64})
 
 Calculate the centroids of the clusters in the raster.
 Depot included as cluster 0.
@@ -74,7 +74,7 @@ Depot included as cluster 0.
 - `depot` : Coordinates of the depot.
 
 # Returns
-A DataFrame containing the cluster ID, latitude, and longitude of the centroids.
+A DataFrame containing the ID, latitude, and longitude of the centroids.
 """
 function centroids(clusters::Raster{Int16, 2}, depot::Tuple{Float64, Float64})
     unique_clusters = unique(clusters)
@@ -84,21 +84,45 @@ function centroids(clusters::Raster{Int16, 2}, depot::Tuple{Float64, Float64})
 
     coordinates = [(x, y) for x in clusters.dims[1], y in clusters.dims[2]]
 
-    cluster_centroids = DataFrame(cluster_id = Int[], lat = Float64[], lon = Float64[])
+    cluster_centroids = DataFrame(id = Int[], lat = Float64[], lon = Float64[])
     # Add depot as cluster 0
     push!(cluster_centroids, (0, depot[1], depot[2]))
 
     # Add each cluster centroid to the DataFrame
-    for cluster_id in unique_clusters
-        indices = findall(x -> x == cluster_id, clusters)
+    for id in unique_clusters
+        indices = findall(x -> x == id, clusters)
 
         lat = mean([coordinates[i][2] for i in indices])
         lon = mean([coordinates[i][1] for i in indices])
 
-        push!(cluster_centroids, (cluster_id, lat, lon))
+        push!(cluster_centroids, (id, lat, lon))
     end
 
-    # Sort the cluster centroids by cluster_id
-    sort!(cluster_centroids, :cluster_id)
+    # Sort the centroids by id
+    sort!(cluster_centroids, :id)
+    return cluster_centroids
+end
+function centroids(clusters::Raster{Int16, 2})
+    unique_clusters = unique(clusters)
+
+    # Remove the zero cluster ID (if present)
+    unique_clusters = unique_clusters[unique_clusters .!= 0]
+
+    coordinates = [(x, y) for x in clusters.dims[1], y in clusters.dims[2]]
+
+    cluster_centroids = DataFrame(id = Int[], lat = Float64[], lon = Float64[])
+
+    # Add each cluster centroid to the DataFrame
+    for id in unique_clusters
+        indices = findall(x -> x == id, clusters)
+
+        lat = mean([coordinates[i][2] for i in indices])
+        lon = mean([coordinates[i][1] for i in indices])
+
+        push!(cluster_centroids, (id, lat, lon))
+    end
+
+    # Sort the centroids by id
+    sort!(cluster_centroids, :id)
     return cluster_centroids
 end
