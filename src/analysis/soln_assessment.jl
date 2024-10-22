01b34189e1b52f691c2fa1a198c2a5cb037206a3
Ryu <r.lippmann@aims.gov.au>
2024-10-22 15:22:08 +1100

    Update initial_solution()
    - Remove plot calls
    - Overloaded initial_solution() to return
    clusts, ms_exclusions, t_exclusions

--- src/analysis/soln_assessment.jl
+++ src/analysis/soln_assessment.jl
@@ -11,7 +11,7 @@ function initial_solution(clusts::Vector{Cluster}, ms_exclusions::DataFrame, t_e
     cluster_centroids_df = DataFrame(id = Int[0], lon = Float64[depot[1]], lat = Float64[depot[2]]) # DataFrame(id = Int[], lon = Float64[], lat = Float64[]) # push!(cluster_centroids_df, (0, depot[1], depot[2]))
     [push!(cluster_centroids_df, (i, clust.attributes.centroid[1], clust.attributes.centroid[2])) for (i, clust) in enumerate(clusts)] #clusters)]
     #####################
-    plot_centroids_and_exclusions(cluster_centroids_df, ms_exclusions, 10)
+    # plot_centroids_and_exclusions(cluster_centroids_df, ms_exclusions, 10)
 
     # Nearest Neighbour to generate initial mothership route & matrix
     ms_soln_NN, ms_feasible_matrix = nearest_neighbour(cluster_centroids_df, ms_exclusions)
@@ -19,7 +19,7 @@ function initial_solution(clusts::Vector{Cluster}, ms_exclusions::DataFrame, t_e
 
     # 2-opt to improve the NN soln
     ms_soln_2opt = two_opt(ms_soln_NN.cluster_sequence, ms_feasible_matrix)
-    plot_waypoints_and_exclusions(ms_soln_2opt.route, ms_soln_2opt.cluster_sequence, ms_exclusions, 10)
+    # plot_waypoints_and_exclusions(ms_soln_2opt.route, ms_soln_2opt.cluster_sequence, ms_exclusions, 10)
 
     #Refs to `clusts` rather than `clusters` because `clusts` have sub-cluster nodes
     clust_seq = [i for i in ms_soln_2opt.cluster_sequence.id if i!==0 && i <= length(clusts)]
@@ -52,7 +52,7 @@ function initial_solution()
     clusters_raster, ms_exclusions, t_exclusions = load_problem()
     clusts = create_clusters(clusters_raster)
 
-    return initial_solution(clusts, ms_exclusions, t_exclusions)
+    return initial_solution(clusts, ms_exclusions, t_exclusions), clusts, ms_exclusions, t_exclusions
 end
 
 function total_dist(soln::MSTSolution)
