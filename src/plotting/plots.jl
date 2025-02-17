91ee984bd6d314dfd14d753618372e2068595480
Ryu <r.lippmann@aims.gov.au>
2025-02-17 15:30:28 +1100

    `clusters!()` labels color to match cluster

--- src/plotting/plots.jl
+++ src/plotting/plots.jl
@@ -133,7 +133,7 @@ function clusters!(
             scatter!(ax, [center_lon], [center_lat], markersize = 10, color = (color, 0.2), strokewidth = 0)
         end
         if labels
-            text!(ax, center_lon, center_lat, text = string(seq), align = (:center, :center), color = :black)
+            text!(ax, center_lon, center_lat, text = string(seq), align = (:center, :center), color = color)
         end
     end
     return ax
