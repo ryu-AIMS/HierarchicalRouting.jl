db30e91ce1586652269dd5d6c5111f0988a330f7
Ryu <r.lippmann@aims.gov.au>
2025-04-02 13:23:32 +1100

    Move `process_targets()`
    `process_targets()`:
    `processing/reads.jl` to `clustering/clustering.jl`
    squash

--- src/clustering/clustering.jl
+++ src/clustering/clustering.jl
@@ -132,3 +132,63 @@ function generate_target_clusters(
 
     return calculate_cluster_centroids(cluster_raster)
 end
+
+"""
+    process_targets(
+        targets::Targets,
+        k::Int8,
+        cluster_tolerance::Float64,
+        suitable_targets_all_path::String,
+        suitable_threshold::Float64,
+        target_subset_path::String,
+        subset::DataFrame,
+        EPSG_code::Int16
+    )::Raster{Int64}
+
+Generate a clustered targets raster by reading in the suitable target location data,
+applying thresholds and cropping to a target subset, and then clustering.
+
+# Arguments
+- `targets`: The targets object containing the target geometries.
+- `k`: The number of clusters.
+- `cluster_tolerance`: The cluster tolerance.
+- `suitable_threshold`: The suitable targets threshold.
+- `target_subset_path`: The path to the target subset raster.
+- `subset`: The DataFrame containing the study area boundary.
+- `EPSG_code`: The EPSG code for the study area.
+
+# Returns
+The clustered targets raster, classified by cluster ID number.
+"""
+function process_targets(
+    targets::Targets,
+    k::Int8,
+    cluster_tolerance::Float64,
+    suitable_threshold::Float64,
+    target_subset_path::String,
+    subset::DataFrame,
+    EPSG_code::Int16
+)::Raster{Int64}
+    if endswith(targets.path, ".geojson")
+        suitable_targets_all = process_geometry_targets(
+            targets,
+            EPSG_code
+        )
+    else
+        suitable_targets_all = process_raster_targets(
+            targets.path,
+            EPSG_code,
+            suitable_threshold
+        )
+    end
+
+    suitable_targets_subset::Raster = Rasters.crop(suitable_targets_all, to=subset.geom)
+    if !isfile(target_subset_path)
+        write(target_subset_path, suitable_targets_subset; force=true)
+    end
+
+    clustered_targets::Raster{Int64, 2} = apply_kmeans_clustering(
+        suitable_targets_subset, k; tol=cluster_tolerance
+    )
+    return clustered_targets
+end
