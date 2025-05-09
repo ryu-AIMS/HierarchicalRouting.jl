9f86afa797347f5f348f75a31aceeb0488ca2227
Ryu <r.lippmann@aims.gov.au>
2025-05-09 15:17:42 +1000

    Adds test data input files
    Adds input files to test loading scenarios.
    Updates `load_problem()` function call to match

--- test/problem_initialization.jl
+++ test/problem_initialization.jl
@@ -1,13 +1,32 @@
 @testset "Initialize problem" begin
     @testset "Initialize with geojson file" begin
-        _target_scenario_folder = "../data/targets/scenarios"
-        _target_scenario_name = "output_slopes_3-10m"
-        _target_scenario_path=joinpath(
-            _target_scenario_folder,
-            _target_scenario_name*".geojson"
-        )
+        _target_scenario_path="test_target_locations.geojson"
+        _subset_path="test_target_area.gpkg"
+        _bathy_path="test_bathy.tif"
+        _wave_disturbance_path="test_env_disturbances.geojson"
 
-        problem = HierarchicalRouting.load_problem(_target_scenario_path)
+        depot = Point{2, Float64}(146.175, -16.84)
+        draft_ms = -10.0,
+        draft_t = -5.0,
+        weight_ms = Float16(5.0)
+        weight_t = Float16(2.0)
+        n_tenders = Int8(3)
+        t_cap = Int16(2)
+
+        problem = HierarchicalRouting.load_problem(
+        _target_scenario_path,
+        _subset_path,
+        _bathy_path,
+        _wave_disturbance_path,
+        depot,
+        draft_ms,
+        draft_t,
+        weight_ms,
+        weight_t,
+        n_tenders,
+        t_cap,
+    );
+
         @test problem isa HierarchicalRouting.Problem
         @test problem.scenario_name == _target_scenario_name
     end
