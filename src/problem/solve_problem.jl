38b6db221387f47194b1ef2e5fde03d5ca8b2f8e
Ryu <r.lippmann@aims.gov.au>
2025-05-22 11:16:37 +1000

    Refactors disturbance handling to generate tender sorties

--- src/problem/solve_problem.jl
+++ src/problem/solve_problem.jl
@@ -61,61 +61,63 @@ function initial_solution(
     ms_soln_sets[1] = ms_route
     tender_soln_sets[1] = initial_tenders
 
-    i = 1
     disturbance_index = 1
-    while i <= length(clust_seq)
-        cluster_id::Int64 = clust_seq[i];
-        if i ∈ disturbance_clusters
-            @info "Disturbance event at $(ms_route.route.nodes[2i-1]): before $(i)th cluster_id=$(cluster_id)"
-            disturbance_index += 1
-            clusters = vcat(
-                clusters[clust_seq][1:i-1],
-                disturb_clusters(
-                    clusters[clust_seq][i:end],
-                    problem.targets.disturbance_gdf,
-                    ms_route.route.nodes[2i-1],
-                    problem.tenders.exclusion
-                )
+    for i ∈ sort(collect(disturbance_clusters))
+        @info "Disturbance event at $(ms_route.route.nodes[2i-1])" *
+            "before $(i)th cluster_id=$(clust_seq[i])"
+        disturbance_index += 1
+        clusters = vcat(
+            clusters[clust_seq][1:i-1],
+            disturb_clusters(
+                clusters[clust_seq][i:end],
+                problem.targets.disturbance_gdf,
+                ms_route.route.nodes[2i-1],
+                problem.tenders.exclusion
             )
+        )
 
-            removed_nodes = setdiff(
-                vcat([c.nodes for c in cluster_sets[disturbance_index-1]]...),
-                vcat([c.nodes for c in clusters]...)
-            )
-            if !isempty(removed_nodes)
-                @info "Removed nodes due to disturbance event (since previous cluster):\n" *
-                "\t$(join(removed_nodes, "\n\t"))"
-            end
+        removed_nodes = setdiff(
+            vcat([c.nodes for c in cluster_sets[disturbance_index-1]]...),
+            vcat([c.nodes for c in clusters]...)
+        )
+        if !isempty(removed_nodes)
+            @info "Removed nodes due to disturbance event (since previous cluster):\n" *
+            "\t$(join(removed_nodes, "\n\t"))"
+        end
 
-            sort!(clusters, by = x -> x.id)
-        cluster_sets[disturbance_index] = clusters
+        sort!(clusters, by = x -> x.id)
 
-            cluster_centroids_df = generate_cluster_df(clusters, problem.depot)
+        cluster_centroids_df = generate_cluster_df(clusters, problem.depot)
 
-            ms_soln_sets[disturbance_index] = ms_route = optimize_mothership_route(
-                problem,
-                cluster_centroids_df,
-                i,
-                ms_route,
+        ms_route = optimize_mothership_route(
+            problem,
+            cluster_centroids_df,
+            i,
+            ms_route,
                 getfield.(clusters[clust_seq][1:i-1], :id)
             )
             clust_seq = filter(
                 i -> i != 0 && i <= length(clusters),
                 ms_route.cluster_sequence.id
-            );
+        );
+
+        cluster_sets[disturbance_index] = clusters
+        ms_soln_sets[disturbance_index] = ms_route
+        current_tender_soln = Vector{TenderSolution}(undef, length(clust_seq))
+        for j in 1:length(clust_seq)
+            if j < i
+                current_tender_soln[j] = tender_soln_sets[disturbance_index-1][j]
+            else
+                current_tender_soln[j] = tender_sequential_nearest_neighbour(
+                    clusters[clust_seq][j],
+                    (ms_route.route.nodes[2j], ms_route.route.nodes[2j+1]),
+                    problem.tenders.number,
+                    problem.tenders.capacity,
+                    problem.tenders.exclusion
+                )
+            end
+            tender_soln_sets[disturbance_index] = current_tender_soln
         end
-
-        start_waypoint::Point{2, Float64} = ms_route.route.nodes[2i]
-        end_waypoint::Point{2, Float64} = ms_route.route.nodes[2i + 1]
-        cluster::Cluster = clusters[clust_seq][i]
-        @info "$(i): Clust $(cluster.id) from $(start_waypoint) to $(end_waypoint)"
-
-        tender_soln_sets[i] = tender_sequential_nearest_neighbour(
-            cluster,
-            (start_waypoint, end_waypoint),
-            problem.tenders.number, problem.tenders.capacity, problem.tenders.exclusion
-        )
-        i+=1
     end
 
     return MSTSolution(cluster_sets, ms_soln_sets, tender_soln_sets)
