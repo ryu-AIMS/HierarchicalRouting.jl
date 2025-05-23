4ec3933a3ce9db2e423d04649177b2928876220e
Ryu <r.lippmann@aims.gov.au>
2025-05-23 10:26:04 +1000

    Applies 2-opt to modified tenders in the `perturb_swap_solution()`
    Applies 2-opt to modified tenders in the `perturb_swap_solution()` to improve possible perturbations to sortie sequences after swapping. This enhances the optimization process by improving tender routes immediately after modification, potentially leading to better solutions in fewer iterations.

--- src/optimization/solution_assessment.jl
+++ src/optimization/solution_assessment.jl
@@ -110,14 +110,21 @@ function perturb_swap_solution(
         vcat(linestrings[2]...)
     )
 
-    tenders_all::Vector{TenderSolution} = copy(soln.tenders[end])
-    tenders_all[clust_seq_idx] = TenderSolution(
+    tender_new = TenderSolution(
         tender.id,
         tender.start,
         tender.finish,
         sorties,
         tender.dist_matrix  #? recompute
     )
+    tender_improved = two_opt(
+        tender_new,
+        exclusions_tender
+    )
+
+    tenders_all::Vector{TenderSolution} = copy(soln.tenders[end])
+    tenders_all[clust_seq_idx] = tender_improved
+
     tenders_full_updated::Vector{Vector{TenderSolution}} = [
         copy(soln.tenders[end]),
         tenders_all
@@ -246,22 +253,35 @@ function perturb_swap_solution(
         for i in 1:length(tender_b.sorties)
     ]
 
-    # Update tenders with existing start/finish (not yet adjusted)
-    tenders_all::Vector{TenderSolution} = copy(soln.tenders[end])
-    tenders_all[clust_a_seq_idx] = TenderSolution(
+    tender_a_new = TenderSolution(
         tender_a.id,
         tender_a.start,
         tender_a.finish,
         sorties_a,
         tender_a.dist_matrix
     )
-    tenders_all[clust_b_seq_idx] = TenderSolution(
+    tender_b_new = TenderSolution(
         tender_b.id,
         tender_b.start,
         tender_b.finish,
         sorties_b,
         tender_b.dist_matrix
     )
+    # Re-run two-opt on the modified sorties
+    tender_a_improved = two_opt(
+        tender_a_new,
+        exclusions_tender
+    )
+    tender_b_improved = two_opt(
+        tender_b_new,
+        exclusions_tender
+    )
+
+    # Update tenders with existing start/finish (not yet adjusted)
+    tenders_all::Vector{Vector{TenderSolution}} = copy(soln.tenders[end])
+    tenders_all[clust_a_seq_idx] = tender_a_improved
+    tenders_all[clust_b_seq_idx] = tender_b_improved
+
     tenders_full_updated::Vector{Vector{TenderSolution}} = [
         copy(soln.tenders[end]),
         tenders_all
