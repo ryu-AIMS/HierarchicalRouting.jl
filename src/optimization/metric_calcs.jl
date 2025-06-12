821c7b64cb860e374ba3c187d0384f148c99cfce
Ryu <r.lippmann@aims.gov.au>
2025-06-12 16:27:59 +1000

    Updates `critical_path()`
    Implements `Float16` vessel weightings for consistency and by slices mothership route for partial solutions in the case of disturbance events.

--- src/optimization/metric_calcs.jl
+++ src/optimization/metric_calcs.jl
@@ -165,7 +165,7 @@ end
 """
     critical_path(
         soln::MSTSolution,
-        vessel_weightings::NTuple{2, Float64}=(1.0, 1.0)
+        vessel_weightings::NTuple{2, Float16}=(1.0, 1.0)
     )::Float64
 
 Compute the critical path cost of the solution.
@@ -185,18 +185,20 @@ function critical_path(
     soln::MSTSolution,
     vessel_weightings::NTuple{2, Float16}=(Float16(1.0), Float16(1.0))
 )::Float64
+    num_clusters = length(soln.tenders[end])
     # Within clusters
     cluster_sorties = tender_clust_dist.(soln.tenders[end])
     cluster_sorties = map(x -> isempty(x) ? [0.0] : x, cluster_sorties)
     longest_sortie_cost = maximum.(cluster_sorties) .* vessel_weightings[2]
-    mothership_sub_clust_cost = mothership_dist_within_clusts(soln.mothership_routes[end].route) *
-        vessel_weightings[1]
+    mothership_sub_clust_cost = vessel_weightings[1] *
+        mothership_dist_within_clusts(soln.mothership_routes[end].route)[1:num_clusters]
 
     cluster_cost_each = max.(longest_sortie_cost, mothership_sub_clust_cost)
     cluster_cost_total = sum(cluster_cost_each)
 
     # Between clusters
-    tow_cost = mothership_dist_between_clusts(soln.mothership_routes[end].route) * vessel_weightings[1]
+    tow_cost = vessel_weightings[1] *
+        mothership_dist_between_clusts(soln.mothership_routes[end].route, num_clusters)
 
     return cluster_cost_total + tow_cost
 end
