d9b09aae2ca12c81ea8cb841a34a1424bee29396
Takuya Iwanaga <takuyai@gmail.com>
2025-07-21 19:01:29 +1000

    Correct display of tender routes

--- src/show.jl
+++ src/show.jl
@@ -101,39 +101,29 @@ function Base.show(io::IO, mime::MIME"text/plain", routes::Vector{Route})
     )
 
     for (i, r) in enumerate(routes)
-        waypoints = r.nodes
+        println(io, "Route $i")
+        show(io, mime, r)
 
-        println(
-            io,
-            """
-            Route $i
-
-            Start: $(waypoints[1])
-            """)
-
-        println(io, "Path:")
-
-        # Get output of linestrings exactly as how it would normally be shown in the REPL
-        io_capture = IOBuffer()
-        show(
-            IOContext(io_capture, :color => true, :limit => true),
-            MIME("text/plain"),
-            GI.coordinates.(r.line_strings)
-        )
-        println(String(take!(io_capture)))
-        print("\n")
-
-        if length(waypoints) > 1
-            println(
-                io,
-                """
-                End: $(waypoints[2])
-                """
-            )
-        else
-            println("No ending waypoint")
-        end
-
-        println("------\n")
+        println("\n------\n")
     end
+end
+
+function Base.show(io::IO, mime::MIME"text/plain", route::Route)
+
+    waypoints = route.nodes
+
+    for (i, w) in enumerate(waypoints)
+        println("Deployment Location $i: $w")
+    end
+
+    println(io, "Route to/from deployment locations:")
+
+    # Get output of linestrings exactly as how it would normally be shown in the REPL
+    io_capture = IOBuffer()
+    show(
+        IOContext(io_capture, :color => true, :limit => true),
+        MIME("text/plain"),
+        GI.coordinates.(route.line_strings)
+    )
+    println(String(take!(io_capture)))
 end
